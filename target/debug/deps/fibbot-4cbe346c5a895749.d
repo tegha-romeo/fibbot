310bab30695237f166b6c95251cf23356815435e
tegha-romeo <romeotegha@gmail.com>
2025-02-25 08:26:41 +0100

    Initial Repo setup

---
+++ target/debug/deps/fibbot-4cbe346c5a895749.d
@@ -0,0 +1,5 @@
+/home/dzeco/Documents/Projects/rust-projects/fibbot/target/debug/deps/libfibbot-4cbe346c5a895749.rmeta: src/main.rs
+
+/home/dzeco/Documents/Projects/rust-projects/fibbot/target/debug/deps/fibbot-4cbe346c5a895749.d: src/main.rs
+
+src/main.rs:
